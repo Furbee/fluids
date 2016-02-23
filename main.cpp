65fe7862739a791561a92255e34038bca481d460
zanshi <zanshi@live.se>
2016-02-23 10:31:24 +0100

    add opengl boilerplate

--- main.cpp
+++ main.cpp
@@ -1,8 +1,64 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
+#include <GL/glew.h>
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+#define GLSL(src) #src
+
+static void error_callback(int error, const char *description) {
+  fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+static void render(GLFWwindow *window) {
+}
+
+int main(void) {
+  GLFWwindow *window;
+
+  glfwSetErrorCallback(error_callback);
+
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  glfwMakeContextCurrent(window);
+
+  glfwSetKeyCallback(window, key_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    render(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+    //glfwWaitEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
