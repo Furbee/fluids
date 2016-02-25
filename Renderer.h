c2eb629a5d40d7865984697535241fdeb730bf89
Oscar Nord <oscar.nord@gmail.com>
2016-02-25 15:35:41 +0100

    Render update

--- Renderer.h
+++ Renderer.h
@@ -5,11 +5,32 @@
 
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 
 class Renderer {
-    
-    
-    
+
+public:
+    Renderer();
+
+    static void draw();
+
+
+private:
+
+    //Functions
+
+    static void error_callback(int error, const char *description);
+
+    static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+    static void render(GLFWwindow *window);
 
 };
 
